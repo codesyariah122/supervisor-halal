242a0448b675ebad7277c8afdd546c78c0c23840
codesyariah122 <pujiermanto@gmail.com>
2024-12-15 01:37:49 +0700

    update/index

--- pages/index.vue
+++ pages/index.vue
@@ -97,11 +97,11 @@ useSeoMeta({
   title: "Halal Kita",
   ogTitle: "Halal Kita",
   description:
-    "kami Halal Kita sebagai Supervisor Halal Indonesia, Web Development, dan seputar solusi digital, kami membantu memimpin Anda melalui perjalanan digital, mulai dari perencanaan hingga pelaksanaan. Dengan pengetahuan mendalam tentang teknologi terbaru dan praktik terbaik, kami membawa solusi yang inovatif dan terukur untuk membantu bisnis Anda mencapai tujuan online mereka dengan percaya diri...",
+    "Supervisor Halal Indonesia adalah lembaga yang berkomitmen untuk memastikan kehalalan produk yang dipasarkan di Indonesia. Kami mendukung perusahaan dalam proses sertifikasi halal, memberikan bimbingan yang transparan, dan memastikan standar halal yang sesuai dengan syariat Islam.",
   ogDescription:
-    "kami Halal Kita sebagai Supervisor Halal Indonesia, Web Development, dan seputar solusi digital, kami membantu memimpin Anda melalui perjalanan digital, mulai dari perencanaan hingga pelaksanaan. Dengan pengetahuan mendalam tentang teknologi terbaru dan praktik terbaik, kami membawa solusi yang inovatif dan terukur untuk membantu bisnis Anda mencapai tujuan online mereka dengan percaya diri",
+    "Supervisor Halal Indonesia adalah lembaga yang berkomitmen untuk memastikan kehalalan produk yang dipasarkan di Indonesia. Kami mendukung perusahaan dalam proses sertifikasi halal, memberikan bimbingan yang transparan, dan memastikan standar halal yang sesuai dengan syariat Islam.",
   ogImage:
-    "https://images.unsplash.com/photo-1519389950473-47ba0277781c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "/logo-site.png",
   twitterCard: "summary_large_image",
 });
 </script>
